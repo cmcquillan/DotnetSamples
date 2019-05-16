645320e71a6e80c704f2daab7621f2a0f987d0b5
Casey McQuillan <Casey@quilltechnologies.com>
2019-05-16 16:30:09 -0700

    Adding ThreadPool example.

---
+++ ThreadPools/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Last thread.  Demonstrates using a reset event.
+            var state = new WorkerState()
+            {
+                ResetEvent = new ManualResetEvent(false),
+                State = "Hello, World!"
+            };
+            ThreadPool.QueueUserWorkItem(Callback, state);
+            WaitHandle.WaitAll(new WaitHandle[] { state.ResetEvent });
+        }
+
+        static void Callback(object state)
+        {
+            WorkerState data = (WorkerState)state;
+            Console.WriteLine(data?.State);
+            data.ResetEvent.Set();
+        }
+    }
+
+    public class WorkerState
+    {
+        public ManualResetEvent ResetEvent { get; set; }
+
+        public object State { get; set; }
+    }
+}
